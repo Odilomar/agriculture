15d82c3e9b9fddec313219e6861e316bb3f720bb
Odilomar Jr <odilomar.junior@gmail.com>
2024-08-13 08:11:20 -0300

    fix: change database provider strutucture

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,10 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './infra/database/database.module';
+import { RuralProducerModule } from './modules/rural-producer/rural-producer.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: false,
+      poolSize: 5,
+      extra: {
+        connectionLimit: 5,
+        idleTimeoutMillis: 10000,
+      },
+    }),
+    RuralProducerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
