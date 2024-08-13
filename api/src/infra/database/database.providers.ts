293650d45b85dd9793087f1bc3774d42ff3832b4
Odilomar Jr <odilomar.junior@gmail.com>
2024-08-13 07:18:12 -0300

    fix: add data source and database provider

--- api/src/infra/database/database.providers.ts
+++ api/src/infra/database/database.providers.ts
@@ -1,20 +1,9 @@
-import { DataSource } from 'typeorm';
+import dataSource from './postgres-data-source';
 
 export const databaseProviders = [
   {
     provide: 'DATA_SOURCE',
     useFactory: async () => {
-      const dataSource = new DataSource({
-        type: 'postgres',
-        host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT) || 5432,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: Boolean(process.env.TYPEORM_SYNCRONIZE) || false,
-      });
-
       return dataSource.initialize();
     },
   },
