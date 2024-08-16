48d742309fcf3dc33cc94b320b15c9fe201c5cac
Odilomar Jr <odilomar.junior@gmail.com>
2024-08-16 10:52:29 -0300

    feat: add delete endpoint

--- api/src/modules/rural-producer/controllers/rural-producer.controller.ts
+++ api/src/modules/rural-producer/controllers/rural-producer.controller.ts
@@ -1,13 +1,16 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
+  Param,
   Post,
 } from '@nestjs/common';
 import {
   CreateRuralProducerService,
+  DeleteRuralProducerService,
   GetDashboardIntelService,
 } from '../use-cases';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
@@ -19,18 +22,35 @@ export class RuralProducerController {
   constructor(
     private readonly createRuralProducerUseCase: CreateRuralProducerService,
     private readonly getDashboardIntelService: GetDashboardIntelService,
+    private readonly deleteRuralProducerService: DeleteRuralProducerService,
   ) {}
 
   @Post('/')
   @HttpCode(HttpStatus.CREATED)
-  @ApiResponse({ status: HttpStatus.CREATED })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Will create the rural producer',
+  })
   async createRuralProducer(@Body() body: CreateRuralProducerDto) {
-    return await this.createRuralProducerUseCase.execute(body);
+    await this.createRuralProducerUseCase.execute(body);
   }
 
   @Get('/dashboard')
-  @ApiResponse({ status: HttpStatus.OK, type: GetDashboardIntelResponseDto })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: GetDashboardIntelResponseDto,
+    description: 'Will get dashboard data',
+  })
   async getDashboard(): Promise<GetDashboardIntelResponseDto> {
     return await this.getDashboardIntelService.execute();
+  }
+
+  @Delete('/:id')
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Will delete the rural producer by id',
+  })
+  async deleteRuralProducerById(@Param('id') id: number) {
+    await this.deleteRuralProducerService.execute(id);
   }
 }
